78119d24061c36fc1494dc416cd93f9c1eb98c30
expert26111 <djoana.v@gmail.com>
2023-12-03 04:02:20 +0100

    adding solution to Single_Number

---
+++ Single_Number/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Single_Number;
+
+
+//int[] nums = { 2, 2, 1 };
+//int[] nums = { 4, 1, 2, 1, 2 };
+int[] nums = { 1 };
+Solution sol = new Solution();
+Console.WriteLine("The solution is: {0}", sol.SingleNumber(nums));
---
+++ Single_Number/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Number
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Dictionary<int,int> result = new Dictionary<int,int>();
+            foreach (int num in nums) 
+            {
+                if(result.ContainsKey(num))
+                {
+                    result.Remove(num);
+                }
+                else
+                {
+                    result.Add(num, -1);
+                }
+            }
+            return result.Keys.First();
+        }
+    }
+}
