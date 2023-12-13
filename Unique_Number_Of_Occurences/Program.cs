5eecc21fd5bddf0682370a4352f14e691b8b78c4
expert26111 <djoana.v@gmail.com>
2023-12-13 03:46:54 +0100

    adding code for the Unique_Number_Of_Occurences

---
+++ Unique_Number_Of_Occurences/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+Console.WriteLine("Hello, World!");
+
+bool UniqueOccurrences(int[] arr)
+{
+    Dictionary<int, int> occurences = new Dictionary<int, int>();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        int current = arr[i];
+        occurences[current] = occurences.GetValueOrDefault(current, 0) + 1;
+    }
+    List<int>[] bucket= new List<int>[arr.Length+1];
+    for(int i = 0; i < bucket.Length; i++) { bucket[i] = new List<int>(); }
+    foreach (KeyValuePair<int, int> entry in occurences)
+    {
+        bucket[entry.Value].Add(1);
+        if (bucket[entry.Value].Count > 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
