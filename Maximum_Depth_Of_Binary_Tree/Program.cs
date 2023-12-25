9341a937b335a02320194d56a067d1a37efaa4eb
expert26111 <djoana.v@gmail.com>
2023-12-25 22:18:23 +0100

    adding exerc

--- Maximum_Depth_Of_Binary_Tree/Program.cs
+++ Maximum_Depth_Of_Binary_Tree/Program.cs
@@ -1,10 +1,78 @@
 ﻿
 
 
+
+
+using System;
+using System.Collections.Generic;
+
+
 Console.WriteLine("Hello, World!");
 
+ bool IsSymmetric(TreeNode root)
+{
+    if(root!= null)
+    {
+        return Compare(root.left,root.right);
+    }
+    else
+    {
+        return false;
+    }
+    bool Compare(TreeNode? p, TreeNode? q)
+    {
+        if (p == null && q == null) { return true; }
+        else if (p != null && q == null) { return false; }
+        else if (p == null && q != null) { return false; }
+        else if (p.val == q.val)
+        {
+            return Compare(p.left, q.right) &&
+            Compare(p.right, q.left);
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+bool IsSameTree(TreeNode p, TreeNode q)
+{
+    // IList<int> listP = InorderTraversal(p);
+    // IList<int> listQ = InorderTraversal(q);
+    // return  listP.SequenceEqual(listQ);
 
- int MaxDepth(TreeNode root)
+    if (p == null && q == null)
+    {
+        return true;
+    }
+    else if (p == null || q == null)
+    {
+        return false;
+    }
+    if (p.val != q.val)
+    {
+        return false;
+    }
+    return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+
+}
+
+
+IList<int> InorderTraversal(TreeNode root)
+{
+    IList<int> list = new List<int>();
+    populateList(root);
+    void populateList(TreeNode node)
+    {
+        if (node == null) { return; }
+        if (node.left != null) { populateList(node.left); }
+        list.Add(node.val);
+        if (node.right != null) { populateList(node.right); }
+    }
+    return list;
+}
+
+int MaxDepth(TreeNode root)
 {
     if(root!=null)
     {
