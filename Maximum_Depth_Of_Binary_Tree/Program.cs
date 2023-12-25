58a87223c4a4639dc6b4b8f86c293485a28f08a5
expert26111 <djoana.v@gmail.com>
2023-12-25 16:56:19 +0100

    adding Maximum Depth

--- Maximum_Depth_Of_Binary_Tree/Program.cs
+++ Maximum_Depth_Of_Binary_Tree/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+
 Console.WriteLine("Hello, World!");
+
+
+ int MaxDepth(TreeNode root)
+{
+    if(root!=null)
+    {
+        return Math.Max(1+MaxDepth(root.left), 1+MaxDepth(root.right));
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+ class TreeNode
+{
+     public int val;
+     public TreeNode? left;
+     public TreeNode? right;
+     public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
